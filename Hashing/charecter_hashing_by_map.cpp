1e2c10ee5fc19fa2059d05f7c6a14ea19e48ce37
Rimon <mdraisul.i.rimon@gmail.com>
2025-02-28 12:48:02 +0600

    hashing using map

---
+++ Hashing/charecter_hashing_by_map.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+
+    //pre storing
+    map<char,int>mpp;
+    for(int i=0; i<str.length(); i++)
+    {
+        mpp[str[i]]++;
+    }
+
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        char ch;
+        cin>>ch;
+
+        //fetch
+        
+        cout<<mpp[ch]<<endl;
+    }
+}
