44eb43c44fd506ad3aba157302308289120714a3
Rimon <mdraisul.i.rimon@gmail.com>
2025-03-01 11:31:04 +0600

    find the second largest element

--- Problem solving on arrays/Find_second_largest_element.cpp
+++ Problem solving on arrays/Find_second_largest_element.cpp
@@ -1,29 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int largestElement(vector<int>&arr, int n)
+int secondLargestElement(vector<int>&arr, int n)
 {
     int largest=arr[0];
+    int secondLargest=INT_MIN;
 
-    for(int i=0; i<n; i++)
+    for(int i=1; i<n; i++)
     {
         if(arr[i]>largest)
         {
+            secondLargest=largest;
             largest=arr[i];
         }
     }
 
-    return largest;
+    return secondLargest;
 }
 
 int main()
 {
-    vector<int>vec={1,4,3,5,2};
+    vector<int>vec={1,4,3,5,7,7,9,9,2};
     int n=vec.size();
 
-    int result=largestElement(vec,n);
+    int result=secondLargestElement(vec,n);
 
-    cout<<"Largest element is: "<<result<<endl;
+    cout<<"Second largest element is: "<<result<<endl;
 
     return 0;
 }
