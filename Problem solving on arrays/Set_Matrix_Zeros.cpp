bc020e89c40428717997c9fe8846ce62151f4d85
Rimon <mdraisul.i.rimon@gmail.com>
2025-03-21 16:43:27 +0600

    updated

--- Problem solving on arrays/Set_Matrix_Zeros.cpp
+++ Problem solving on arrays/Set_Matrix_Zeros.cpp
@@ -26,13 +26,66 @@ void setZerosBetter(vector<vector<int>>&vec , int n, int m)
     }
 }
 
+void setZerosOptimal(vector<vector<int>>&vec , int n, int m)
+{
+    int col0=1;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(vec[i][j]==0)
+            {
+                vec[i][0]=0;
+
+                if(j != 0)
+                {
+                    vec[0][j]=0;
+                }
+                else{
+                    col0=0;
+                }
+            }
+        }
+    }
+
+    for(int i=1; i<n; i++)
+    {
+        for(int j=1; j<m; j++)
+        {
+            if(vec[i][j] != 0)
+            {
+                if(vec[0][j]==0 || vec[i][0]==0)
+                {
+                    vec[i][j]=0;
+                }
+            }
+        }
+    }
+
+    if(vec[0][0]==0)
+    {
+        for(int j=0; j<m; j++)
+        {
+            vec[0][j]=0;
+        }
+    }
+    if(col0==0)
+    {
+        for(int i=0; i<n; i++)
+        {
+            vec[i][0]=0;
+        }
+    }
+}
+
 int main()
 {
     vector<vector<int>>vec={{1,1,1,1},{1,0,0,1},{1,1,0,1},{1,1,1,1}};
     int n=vec.size();
     int m=vec[0].size();
 
-    setZerosBetter(vec,n,m);
+    setZerosOptimal(vec,n,m);
 
     for(int i=0; i<vec.size(); i++)
     {
