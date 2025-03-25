8ae2910da5a483f1f945bcf413d9e8080360a4d8
Rimon <mdraisul.i.rimon@gmail.com>
2025-03-25 22:34:19 +0600

    4 sum better approach

--- Problem solving on arrays/4-Sum_Problem.cpp
+++ Problem solving on arrays/4-Sum_Problem.cpp
@@ -30,11 +30,42 @@ vector<vector<int>> fourSumBrute(vector<int>&vec)
     return vec3;
 }
 
+vector<vector<int>> fourSumBetter(vector<int>&vec)
+{
+    set<vector<int>>st;
+    int n=vec.size();
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            set<int>hash;
+            for(int k=j+1; k<n; k++)
+            {
+                int l=-(vec[i]+vec[j]+vec[k]);
+
+                if(hash.find(l) != hash.end())
+                {
+                    vector<int>vec2={vec[i],vec[j],vec[k],l};
+                    sort(vec2.begin(),vec2.end());
+
+                    st.insert(vec2);
+                }
+                hash.insert(vec[k]);
+            }
+        }
+    }
+
+    vector<vector<int>>vec3(st.begin(),st.end());
+
+    return vec3;
+}
+
 int main()
 {
     vector<int>vec={1,0,-1,0,-2,2};
 
-    vector<vector<int>> result=fourSumBrute(vec);
+    vector<vector<int>> result=fourSumBetter(vec);
 
     for(int i=0; i<result.size(); i++)
     {
