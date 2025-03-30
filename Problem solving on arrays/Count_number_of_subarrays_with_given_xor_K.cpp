c86bf5d209860e8cec9f969b193b7fc5641eceae
Rimon <mdraisul.i.rimon@gmail.com>
2025-03-30 19:50:49 +0600

    updated

--- Problem solving on arrays/Count_number_of_subarrays_with_given_xor_K.cpp
+++ Problem solving on arrays/Count_number_of_subarrays_with_given_xor_K.cpp
@@ -42,12 +42,38 @@ int countSubarrayBetter(vector<int>&vec,int k)
     return count;
 }
 
+int countSubarrayOptimal(vector<int>&vec,int k)
+{
+    unordered_map<int,int>mpp;
+    int n=vec.size();
+    int count=0;
+    int XOR=0;
+    mpp[0]=1;
+    for(int i=0; i<n; i++)
+    {
+        // prefix XOR till index i:
+        XOR = XOR ^ vec[i];
+
+        //By formula: x = xr^k:
+        int x = XOR ^ k;
+
+        // add the occurrence of xr^k
+        // to the count:
+        count += mpp[x];
+
+        // Insert the prefix xor till index i
+        // into the map:
+        mpp[XOR]++;
+    }
+    return count;
+}
+
 int main()
 {
     vector<int>vec={4, 2, 2, 6, 4};
     int k=6;
 
-    int number=countSubarrayBetter(vec,k);
+    int number=countSubarrayOptimal(vec,k);
 
     cout<<"Total subarray is: "<<number<<endl;
 
