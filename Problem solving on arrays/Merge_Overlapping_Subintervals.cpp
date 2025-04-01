edbaa194c6e7b83bd3f2de2298fc4e80371d76e2
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-01 13:10:13 +0600

    updated for optimal solution

--- Problem solving on arrays/Merge_Overlapping_Subintervals.cpp
+++ Problem solving on arrays/Merge_Overlapping_Subintervals.cpp
@@ -31,11 +31,29 @@ vector<vector<int>> margeSubintervalsBrute(vector<vector<int>>&nums)
     return ans;
 }
 
+vector<vector<int>> margeSubintervalsOptimal(vector<vector<int>>&intervals)
+{
+    vector<vector<int>>ans;
+    int n=intervals.size();
+    sort(intervals.begin(),intervals.end());
+    for(int i=0; i<n; i++)
+    {
+        if(ans.empty() || intervals[i][0]>ans.back()[1])
+        {
+            ans.push_back(intervals[i]);
+        }
+        else{
+            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+        }
+    }
+    return ans;
+}
+
 int main()
 {
     vector<vector<int>>vec={{1, 3}, {8, 10}, {2, 6}, {15, 18}};
 
-    vector<vector<int>> result=margeSubintervalsBrute(vec);
+    vector<vector<int>> result=margeSubintervalsOptimal(vec);
 
     for (auto it : result) {
         cout << "[" << it[0] << ", " << it[1] << "] ";
