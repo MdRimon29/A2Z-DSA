020fd0243c0c0a79206b61c0f82c299aae66b52c
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-02 18:17:40 +0600

    updated

--- Problem solving on arrays/Find_the_repeating_and_missing_number.cpp
+++ Problem solving on arrays/Find_the_repeating_and_missing_number.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// tc->O(2n)
+// sc->O(n)
 vector<int> findRepeatingAndMissingBetter(vector<int>&nums)
 {
     int n=nums.size();
@@ -31,11 +33,38 @@ vector<int> findRepeatingAndMissingBetter(vector<int>&nums)
     return {repeating,missing};
 }
 
+// tc= O(n)
+// sc= O(1)
+vector<int> findRepeatingAndMissingOptimal1(vector<int>&nums)
+{
+    //S-Sn
+    //S2-S2n
+    int n=nums.size();
+    long long Sn=(n*(n+1))/2;
+    long long S2n=(n*(n+1)*(2*n+1))/6;
+
+    long long S=0,S2=0;
+    for(int i=0; i<n; i++)
+    {
+        S+=nums[i];
+        S2 += nums[i]*nums[i];
+    }
+
+    long long val1=S-Sn;    //x-y
+    long long val2=S2-S2n;  //x^2-y^2
+    val2=val2/val1; //x+y
+
+    long long x=(val1+val2)/2;
+    long long y=x-val1;
+
+    return {(int)x,(int)y};
+}
+
 int main()
 {
     vector<int>vec={1,2,3,2};
 
-    vector<int>result=findRepeatingAndMissingBetter(vec);
+    vector<int>result=findRepeatingAndMissingOptimal1(vec);
     
     cout<<"Repeating number is: "<<result[0]<<endl;
     cout<<"Missing number is: "<<result[1]<<endl;
