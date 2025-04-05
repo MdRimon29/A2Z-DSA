3291f1700d1e3ae8ae6850c8284085dafba91654
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-05 17:41:09 +0600

    how to make a subarray

---
+++ Problem solving on arrays/Maximum_Product_Subarray.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumProductBrute(vector<int>&vec)
+{
+    int maxi=INT_MIN;
+    for(int i=0; i<vec.size(); i++)
+    {
+        for(int j=i; j<vec.size(); j++)
+        {
+            int ans=1;
+            for(int k=i; k<=j; k++)
+            {
+                ans=ans * vec[k];
+            }
+            maxi=max(maxi,ans);
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    vector<int>vec={1,2,3,4,5,0};
+
+    int result=maximumProductBrute(vec);
+
+    cout<<"The result is: "<<result<<endl;
+}
