8675dda7c6dbdafb6d4435d27277eab58623e353
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-05 17:42:56 +0600

    better approach for any subarray types of problem

--- Problem solving on arrays/Maximum_Product_Subarray.cpp
+++ Problem solving on arrays/Maximum_Product_Subarray.cpp
@@ -20,11 +20,27 @@ int maximumProductBrute(vector<int>&vec)
     return maxi;
 }
 
+int maximumProductBetter(vector<int>&vec)
+{
+    int maxi=INT_MIN;
+    for(int i=0; i<vec.size(); i++)
+    {
+        int ans=1;
+        for(int j=i; j<vec.size(); j++)
+        {
+            ans=ans * vec[j];
+            maxi=max(maxi,ans);
+        }
+    }
+
+    return maxi;
+}
+
 int main()
 {
     vector<int>vec={1,2,3,4,5,0};
 
-    int result=maximumProductBrute(vec);
+    int result=maximumProductBetter(vec);
 
     cout<<"The result is: "<<result<<endl;
 }
