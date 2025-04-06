3b5cef8a7953a3aaa93b9bb670b41ccdc24abf08
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-06 09:34:52 +0600

    lower bound using binary search

---
+++ Binary Search/02-Implement_Lower_Bound.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findLowerBoundBrute(vector<int>&vec,int target)
+{
+    int ans=vec.size();
+    for(int i=0; i<=vec.size(); i++)
+    {
+        if(vec[i]>=target)
+        {
+            ans=min(ans,i);
+        }
+    }
+    return ans;
+}
+
+int findLowerBoundOptimal(vector<int>&vec,int target)
+{
+    int low=0,high=vec.size()-1,ans=vec.size();
+    
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(vec[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int>vec={3,5,8,15,19};
+    int x=9;
+
+    int result=findLowerBoundOptimal(vec,x);
+
+    cout<<"Lower bound is: "<<result<<endl;
+
+    return 0;
+}
