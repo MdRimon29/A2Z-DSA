117a283ccfa3d22a6aa60b5a816a8b9f03c4e383
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-06 10:02:01 +0600

    search insert postion problem

---
+++ Binary Search/04-Search_Insert_Position.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findInsertPositionBrute(vector<int>&vec,int target)
+{
+    int ans=vec.size();
+    for(int i=0; i<=vec.size(); i++)
+    {
+        if(vec[i]>=target)
+        {
+            ans=min(ans,i);
+        }
+    }
+    return ans;
+}
+
+int findInsertPositionOptimal(vector<int>&vec,int target)
+{
+    int low=0,high=vec.size()-1,ans=vec.size();
+    
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(vec[mid]>=target){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int>vec={3,5,8,15,19};
+    int x=9;
+
+    int result=findInsertPositionOptimal(vec,x);
+
+    //In coding round
+    int res=lower_bound(vec.begin(),vec.end(),x)-vec.begin();
+
+    cout<<"Insert position is: "<<result<<endl;
+
+    return 0;
+}
