65239a6f91d6c53ab7dc2d4bf6bd0bf871632d49
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-06 08:27:46 +0600

    updated for binary search in two structure

---
+++ Binary Search/01-Binary_Search_to_find_X_in_sorted_array.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int low=0;
+    int high=nums.size()-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else if(nums[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(vector<int>&nums,int low,int high,int target)
+{
+    if(low>high) return -1;
+    int mid=(low+high)/2;
+    if(nums[mid]==target)
+    {
+        return mid;
+    }
+    else if(nums[mid]<target) 
+    {
+        return binarySearch(nums,mid+1,high,target);
+    }
+    else{
+        return binarySearch(nums,low,mid-1,target);
+    }
+}
+int searchRecursion(vector<int>&nums,int target)
+{
+    return binarySearch(nums,0,nums.size()-1,target);
+}
+
+int main()
+{
+    vector<int>nums={-1,0,3,5,9,12};
+    int target = 3;
+
+    int result=searchRecursion(nums,target);
+
+    cout<<result<<endl;
+    return 0;
+}
