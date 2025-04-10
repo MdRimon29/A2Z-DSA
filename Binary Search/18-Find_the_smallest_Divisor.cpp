8d6c8d2cd414367ca3fe052f5d0c5d8c03613363
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-10 12:07:16 +0600

    updated

---
+++ Binary Search/18-Find_the_smallest_Divisor.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sumDivision(vector<int>&nums,int mid)
+{
+    int count=0;
+    for(int i=0; i<nums.size(); i++)
+    {
+        count+= ceil((double)nums[i]/(double)mid);
+    }
+    return count;
+}
+
+int smallestDivisor(vector<int>& nums, int threshold) {
+    int n=nums.size();
+    if(n>threshold) return -1;
+
+    int low=1,high=*max_element(nums.begin(),nums.end());
+
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        int sum=sumDivision(nums,mid);
+
+        if(sum<=threshold)
+        {
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+
+    return low;
+}
+
+int main()
+{
+    vector<int>vec={44,22,33,11,1};
+    int threshold=5;
+
+    int result=smallestDivisor(vec,threshold);
+
+    cout<<result<<endl;
+}
