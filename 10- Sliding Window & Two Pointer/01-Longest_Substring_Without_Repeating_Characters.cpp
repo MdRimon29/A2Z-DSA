44f931aba6561b973839410421799680880616fe
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-15 22:43:16 +0600

    updated

---
+++ 10- Sliding Window & Two Pointer/01-Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    int n=s.size();
+    unordered_map<char,int>last;
+    int left=0,ans=0;
+
+    for(int right=0; right<n; right++)
+    {
+        if(last.count(s[right]))
+        {
+            left=max(left,last[s[right]]+1);
+        }
+
+        ans=max(ans,right-left+1);
+
+        last[s[right]]=right;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    string s="abcabcbb";
+
+    int result=lengthOfLongestSubstring(s);
+
+    cout<<result<<endl;
+
+    return 0;
+}
