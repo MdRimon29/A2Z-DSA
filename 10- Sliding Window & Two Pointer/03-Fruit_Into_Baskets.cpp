d077e6a78ddc78e5b6393500a172ecbcb086fd1b
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-18 19:04:18 +0600

    updated

--- 10- Sliding Window & Two Pointer/03-Fruit_Into_Baskets.cpp
+++ 10- Sliding Window & Two Pointer/03-Fruit_Into_Baskets.cpp
@@ -24,13 +24,31 @@ int FruitsInBasketsBrute(vector<int>& nums, int k){
     return maxlen;
 }
 
-// worst tc->O(n+n)
-// sc->O(1)
+// tc->O(n)
+// sc->O(3) ~ O(1) -->as it is constant size complexity
 int FruitsInBaskets(vector<int>& nums, int k) {
     int left=0,right=0;
     int n=nums.size();
     int sum=0,maxLen=0;
-    
+    unordered_map<int,int>mpp;
+
+    while(right<n)
+    {
+        mpp[nums[right]]++;
+
+        if(mpp.size()>k)
+        {
+            mpp[nums[left]]--;
+            if(mpp[nums[left]]==0){
+                mpp.erase(nums[left]);
+            }
+            left++;
+        }
+
+        if(mpp.size()<=k) maxLen=max(maxLen,right-left+1);
+
+        right++;
+    }
     
 
     return maxLen;
@@ -41,7 +59,7 @@ int main()
     vector<int>vec={3,3,3,1,2,1,1,2,2,3,3};
     int k=2;
 
-    int result=FruitsInBasketsBrute(vec,k);
+    int result=FruitsInBaskets(vec,k);
 
     cout<<result<<endl;
 
