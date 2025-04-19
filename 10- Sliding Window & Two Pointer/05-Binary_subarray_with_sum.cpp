66c038a9b60ef168e109261b95b3edb5d22362bc
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-19 10:36:06 +0600

    updated

---
+++ 10- Sliding Window & Two Pointer/05-Binary_subarray_with_sum.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numSubarraysWithSumGoal(vector<int>& nums, int goal){
+    if(goal<0) return 0;
+
+    int l=0,r=0;
+    int sum=0,count=0;
+
+    while(r<nums.size())
+    {
+        sum += nums[r];
+
+        while(sum>goal)
+        {
+            sum -= nums[l];
+            l++;
+        }
+
+        count += (r-l+1);
+
+        r++;
+    }
+
+    return count;
+}
+
+// tc=O(2*2n)
+// sc=O(1)
+int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int res1=numSubarraysWithSumGoal(nums,goal);
+    int res2=numSubarraysWithSumGoal(nums,goal-1);
+
+    return res1-res2;
+}
+
+int main()
+{
+    vector<int>vec={1,0,1,0,1};
+    int k=2;
+
+    int result=numSubarraysWithSum(vec,k);
+
+    cout<<result<<endl;
+}
