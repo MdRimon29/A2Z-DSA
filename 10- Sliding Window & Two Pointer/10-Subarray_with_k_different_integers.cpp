c60d06865cb8382abb5d86a478c1572647dae6c5
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-23 20:17:22 +0600

    updated

--- 10- Sliding Window & Two Pointer/10-Subarray_with_k_different_integers.cpp
+++ 10- Sliding Window & Two Pointer/10-Subarray_with_k_different_integers.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// tc=O(n**2)
+// sc=O(n)
 int countSubarray(vector<int>&vec,int k)
 {
     unordered_map<int,int>mpp;
@@ -25,12 +26,44 @@ int countSubarray(vector<int>&vec,int k)
     return count;
 }
 
+// tc=O(2*n)
+// sc=O(n)
+int subarrays(vector<int>& nums, int k) {
+    int l=0,r=0;
+    unordered_map<int,int>mpp;
+    int count=0;
+
+    while(r<nums.size())
+    {
+        mpp[nums[r]]++;
+
+        while(mpp.size()>k){
+            mpp[nums[l]]--;
+            if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+            l++;
+        }
+
+        count+=(r-l+1);
+
+        r++;
+    }
+
+    return count;
+}
+
+int subarraysWithKDistinct(vector<int>& nums, int k) {
+    int num1=subarrays(nums,k);
+    int num2=subarrays(nums,k-1);
+
+    return num1-num2;
+}
+
 int main()
 {
     vector<int>vec={1,2,1,2,3};
     int k=2;
 
-    int result=countSubarray(vec,k);
+    int result=subarraysWithKDistinct(vec,k);
 
     cout<<result<<endl;
 }
