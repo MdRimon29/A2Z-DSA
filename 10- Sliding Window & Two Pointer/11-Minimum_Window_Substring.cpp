930fbc483f58ec3da7851d0bf57e36775bf9f2ba
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-24 21:06:42 +0600

    updated for structure

---
+++ 10- Sliding Window & Two Pointer/11-Minimum_Window_Substring.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string minWindow(string s, string t) {
+    int l=0,r=0;
+    unordered_map<string,int>mpp;
+    string str;
+
+    while(r<s.size()){
+        
+    }
+}
+
+int main()
+{
+    string s="ADOBECODEBANC";
+    string t="ABC";
+
+    string result=minWindow(s,t);
+
+    cout<<result<<endl;
+}
