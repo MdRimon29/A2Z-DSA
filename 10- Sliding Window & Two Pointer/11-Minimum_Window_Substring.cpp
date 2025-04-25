495f5fc7a52d3283996d685a4ea8ef16296c68ad
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-25 23:08:51 +0600

    have some issues,need to fixed that

--- 10- Sliding Window & Two Pointer/11-Minimum_Window_Substring.cpp
+++ 10- Sliding Window & Two Pointer/11-Minimum_Window_Substring.cpp
@@ -2,13 +2,37 @@
 using namespace std;
 
 string minWindow(string s, string t) {
-    int l=0,r=0;
-    unordered_map<string,int>mpp;
+    if (s.empty() || t.empty()) return "";
+    
+    int l=0,r=0,n=s.size(),m=t.size();
+    unordered_map<char,int>mpp;
+    int minLen=INT_MAX,start=-1,count=0;
     string str;
 
-    while(r<s.size()){
-        
+    for(int i=0; i<m; i++)
+    {
+        mpp[t[i]]++;
     }
+
+    while(r<s.size()){
+        if(mpp[s[r]]>0){
+            count++;
+            mpp[s[r]]--;
+        }
+
+        while(count==m)
+        {
+            if((r-l+1)<minLen){
+                minLen=min(minLen,(r-l+1));
+                start=l;
+            }
+            mpp[s[l]]--;
+            if(mpp[s[l]]>0) count=count-1;
+        }
+        r=r+1;
+    }
+
+    return start==-1?"":s.substr(start,minLen);
 }
 
 int main()
