15523b0644865ea3543187c8dfc70393afeca8d0
Rimon <mdraisul.i.rimon@gmail.com>
2025-05-18 11:36:17 +0600

    updated

---
+++ Step 09- Stack and Queue/08-Implement_Min_Stack.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//tc=O(1)
+//sc=O(2*n)
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    stack<int>st;
+    stack<int>minmm;
+    
+    void push(int val) {
+        if(minmm.empty()){
+            minmm.push(val);
+        }
+        else if(minmm.top()>=val){
+            minmm.push(val);
+        }
+        else{
+            minmm.push(minmm.top());
+        }
+        st.push(val);
+    }
+    
+    void pop() {
+        st.pop();
+        minmm.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minmm.top();
+    }
+};
+
+int main()
+{
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    cout << "Current min: " << minStack.getMin() << endl; // Returns -3
+    minStack.pop();
+    cout << "Top element: " << minStack.top() << endl;    // Returns 0
+    cout << "Current min: " << minStack.getMin() << endl; // Returns -2
+    return 0;
+}
